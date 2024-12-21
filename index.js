0bcb1167ce7c9e822a619abe7331c729a72c8d77
Tomislav Dukez <tomdu3@ymail.com>
2024-12-21 10:56:43 +0000

    add express server

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const server = require('http').createServer();
+const app = express();
+
+app.get('/', function(req, res) {
+	res.sendFile('index.html', {root: __dirname});
+});
+
+server.on('request', app);
+server.listen(3000, function() { console.log('Server started on PORT 3000'); });
