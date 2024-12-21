bbfd60ba6df26f1629599f92409f57a266ff5cdf
Tomislav Dukez <tomdu3@ymail.com>
2024-12-21 13:53:59 +0000

    fix mispelled code

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-const express = require('express');
-const server = require('http').createServer();
+const express = require("express");
+const server = require("http").createServer();
 const app = express();
 
-app.get('/', function(req, res) {
-	res.sendFile('index.html', {root: __dirname});
+app.get("/", function (req, res) {
+  res.sendFile("index.html", { root: __dirname });
 });
 
-server.on('request', app);
-server.listen(3000, function() { 
-	console.log('Server started on PORT 3000');
+server.on("request", app);
+server.listen(3000, function () {
+  console.log("Server started on PORT 3000");
 });
 
 /** Begin websocker */
 
-const WebSocketServer = require('ws').Server;
+const WebSocketServer = require("ws").Server;
 
-const wss = new WebSocketServer({server: server});
+const wss = new WebSocketServer({ server: server });
 
-wss.on('connection', function connection(ws) {
-	const numClients = wss.clients.size;
-	console.log('Clients connected', numClients);
+wss.on("connection", function connection(ws) {
+  const numClients = wss.clients.size;
+  console.log("Clients connected", numClients);
 
-	wss.broadcast(`Current visitors: ${numClients}`);  // sends message to everybody connected
+  wss.broadcast(`Current visitors: ${numClients}`); // sends message to everybody connected
 
-	if (wss.readyState === wss.OPEN) {
-	wss.send('Welcome to my server');  // message to the client
-	}
+  if (ws.readyState === ws.OPEN) {
+    ws.send("Welcome to my server"); // message to the client
+  }
 
-	wss.on('close', function close() {
-		console.log('Client has disconnected');
-	});
+  ws.on("close", function close() {
+    console.log("Client has disconnected");
+  });
 });
 
 wss.broadcast = function broadcast(data) {
-	wss.clients.forEacj(function each(client) {
-		client.send(data);
-	});
-}
+  wss.clients.forEach(function each(client) {
+    client.send(data);
+  });
+};
